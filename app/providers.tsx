b5be78022655271ae16c591ade86242507639ba8
fsa317 <fsa317@gmail.com>
2023-06-25 16:01:15 -0400

    updates

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from "@/components/mtexport";
 import { SessionProvider } from "next-auth/react";
 
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+    <ThemeProvider>
       {children}
     </ThemeProvider>
     </SessionProvider>
---
+++ components/mtexport.ts
@@ -0,0 +1,5 @@
+"use client";
+ 
+import { ThemeProvider,  Stepper, Step, Button, Typography } from "@material-tailwind/react";
+ 
+export { ThemeProvider,  Stepper, Step, Button, Typography };
